ecc22f6e5bd9b621fc31839fc47bb3b49978094d
Armin Hafner <a_h82@gmx.net>
2020-04-24 23:27:26 +0200

    46 - middleware

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
@@ -14,13 +17,50 @@ router.post('/', [
         min: 6
       })
     ],
-  (req, res) => {
+    async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists'});
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(payload, config.get('jwtSecret'), {
+        expiresIn: 360000
+      }, (err, token) => {
+        if(err) throw err;
+        res.send({ token });
+      });
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json('Server Error');
+    }
   });
 
 module.exports = router;
