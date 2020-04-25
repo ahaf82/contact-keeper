7d7a2d6e0fd557576cc98f0fdd14d8919b928aee
Armin Hafner <a_h82@gmx.net>
2020-04-25 23:45:38 +0200

    React Setup, CleanUp and Navbar

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+        <Fragment className="App">
+          <Navbar />
+          <div className="container">
+            <Switch>
+              < Route exact path="/" component={Home} />
+              < Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const About = props => {
+    return (
+        <div>
+            <h1>About this app</h1>
+            <p className="my-1">
+                This is a full stack React App for keeping contacts
+            </p>
+            <p className="bg-dark p">
+                <strong>Version: </strong>1.0.0
+            </p>    
+        </div>
+    )
+}
+
+About.propTypes = {
+
+}
+
+export default About
