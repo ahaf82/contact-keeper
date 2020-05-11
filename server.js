a90b141e03e8bc267869bf68fa74ea03878e464c
ahaf82 <a_h82@gmx.net>
2020-05-11 02:13:04 +0200

    staging changes

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production') {
-    // Set static folder   
-    app.use(express.static('client/build'));
-
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
+
+    // Set static folder   
+    app.use(express.static('client/build'));
+
 }
 
 const PORT = process.env.PORT || 5000;
