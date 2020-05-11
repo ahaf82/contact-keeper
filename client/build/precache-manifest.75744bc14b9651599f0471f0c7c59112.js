7522271d4cd11e13780c49ecdfd515b61e819af4
ahaf82 <a_h82@gmx.net>
2020-05-11 02:01:29 +0200

    deploy app

---
+++ client/build/precache-manifest.75744bc14b9651599f0471f0c7c59112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c291c909e5f4b57d6941ea72eb41ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a897665e3cf5e4bb165",
+    "url": "/static/css/main.620fe750.chunk.css"
+  },
+  {
+    "revision": "af1b78fc716b8f687868",
+    "url": "/static/js/2.ad8e0ebe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad8e0ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a897665e3cf5e4bb165",
+    "url": "/static/js/main.f98bc0f9.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
